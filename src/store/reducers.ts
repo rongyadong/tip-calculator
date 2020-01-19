cf7fe143d4ba4d733ed0130330e760d2fe021733
JackRong <rongyadong19900724@gmail.com>
2020-01-20 00:33:16 +1100

    add actions.ts and related actions case in rootReducer

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,5 +1,6 @@
-import {Reducer, Action} from 'redux';
+import {Reducer} from 'redux';
 import {StoreState} from '.';
+import {Actions, ActionTypes} from './actions';
 
 const initialState: StoreState = {
     bill: 0,
@@ -7,7 +8,35 @@ const initialState: StoreState = {
     split: 1,
 };
 
-export const rootReducer: Reducer<StoreState, Action> = (
+export const rootReducer: Reducer<StoreState, Actions> = (
     state = initialState,
     action,
-) => state;
+) => {
+    switch (action.type) {
+        case ActionTypes.BillChange:
+            return {
+                ...state,
+                bill: Number(action.payload),
+            };
+        case ActionTypes.PercentageChange:
+            return {
+                ...state,
+                percentage: Number(action.payload),
+            };
+        case ActionTypes.SplitIncrement:
+            return {
+                ...state,
+                split: state.split + 1,
+            };
+        case ActionTypes.SplitDecrement:
+            const split = state.split - 1;
+            return {
+                ...state,
+                split: split > 1 ? split : state.split,
+            };
+        case ActionTypes.Reset:
+            return initialState;
+        default:
+            return state;
+    }
+};
